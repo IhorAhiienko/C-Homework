4fc2eff25a96101bf64a4f2388d79704b8d45b50
IhorAhiienko <98847772+IhorAhiienko@users.noreply.github.com>
2022-02-01 22:25:04 +0200

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,10 @@
+
+// Online IDE - Code Editor, Compiler, Interpreter
+
+#include <stdio.h>
+
+int main()
+{   char var1[]="Hello";
+    char var2[]="World";
+    printf("!!!%s , %s!!!",var1,var2);
+}
