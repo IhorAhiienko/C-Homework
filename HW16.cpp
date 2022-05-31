bb5d8b8b333cd6871ee621b4f689fbf104cda922
IhorAhiienko <98847772+IhorAhiienko@users.noreply.github.com>
2022-06-01 00:22:22 +0300

    HW16

---
+++ HW16.cpp
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+//Task 11.5(ã)
+typedef struct poly_coef {
+    double coef;
+    unsigned deg;
+}poly_coef;
+
+
+typedef struct poly{
+    unsigned m;
+    poly_coef* coefs;
+}poly;
+
+
+int input(poly_coef* r) {
+    printf("deg= ");
+    scanf("%d", &r->deg);
+    printf("coef= ");
+    scanf("%lf", &r->coef);
+    return 0;
+}
+
+
+
+
+int inputpoly(const char* fname) {
+    FILE* f1 = fopen(fname, "a+b");
+    int m;
+    printf("number of monoms=");
+    scanf("%u", &m);
+    fwrite(&m, sizeof(m), 1, f1);
+    poly_coef coef;
+    for (unsigned i = 0; i < m; i++) {
+        input(&coef);
+        fwrite(&coef, sizeof(coef), 1, f1);
+    }
+    fclose(f1);
+    return 0;
+}
